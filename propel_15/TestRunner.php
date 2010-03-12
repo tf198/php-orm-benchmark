1c8ce3578c7abef3a52b97614b0d48fda3eb95d7
FZaninotto <FZaninotto@c3bb4960-20b7-11df-9e62-c93c6248c2ac>
2010-03-12 10:20:13 +0000

    added Propel15aLa14TestSuite
    git-svn-id: http://php-orm-benchmark.googlecode.com/svn/trunk@18 c3bb4960-20b7-11df-9e62-c93c6248c2ac

--- propel_15/TestRunner.php
+++ propel_15/TestRunner.php
@@ -1,5 +1,11 @@
 <?php
 
+require dirname(__FILE__) . '/Propel15aLa14TestSuite.php';
+
+$test = new Propel15aLa14TestSuite();
+$test->initialize();
+$test->run();
+
 require dirname(__FILE__) . '/Propel15TestSuite.php';
 
 $test = new Propel15TestSuite();
