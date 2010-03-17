50c7f908fa90a7dac616513f8872b9de254e7e15
roman.borschel <roman.borschel@c3bb4960-20b7-11df-9e62-c93c6248c2ac>
2010-03-17 16:42:38 +0000

    First version of doctrine2 suite.
    git-svn-id: http://php-orm-benchmark.googlecode.com/svn/trunk@22 c3bb4960-20b7-11df-9e62-c93c6248c2ac

--- TestRunner.php
+++ TestRunner.php
@@ -8,3 +8,4 @@ passthru('php propel_14/TestRunner.php');
 passthru('php propel_15/TestRunner.php');
 passthru('php propel_15_with_cache/TestRunner.php');
 passthru('php doctrine_12/TestRunner.php');
+passthru('php doctrine_2/TestRunner.php');
