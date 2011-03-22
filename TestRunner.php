52e33e99c5089197882d60e0dac2254965fb2647
Tris Forster <tris.git@tfconsulting.com.au>
2011-03-23 10:01:00 +1100

    Added propel submodules

--- TestRunner.php
+++ TestRunner.php
@@ -6,7 +6,7 @@ echo "                               |--------|--------|--------|--------|------
 passthru('php raw_pdo/TestRunner.php');
 passthru('php dormio_01/TestRunner.php');
 passthru('php propel_14/TestRunner.php');
-//passthru('php propel_15/TestRunner.php');
+passthru('php propel_15/TestRunner.php');
 //passthru('php propel_15_with_cache/TestRunner.php');
 //passthru('php doctrine_12/TestRunner.php');
 //passthru('php doctrine_2/TestRunner.php');
