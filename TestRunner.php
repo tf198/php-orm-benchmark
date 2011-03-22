dee8abc926d708434aaaa5fefb5a63f5d4e1885b
Tris Forster <tris.git@tfconsulting.com.au>
2011-03-23 09:56:36 +1100

    Added basic dormio tests

--- TestRunner.php
+++ TestRunner.php
@@ -4,8 +4,9 @@ echo "                               | Insert | findPk | complex| hydrate|  with
 echo "                               |--------|--------|--------|--------|--------|\n";
 
 passthru('php raw_pdo/TestRunner.php');
+passthru('php dormio_01/TestRunner.php');
 passthru('php propel_14/TestRunner.php');
-passthru('php propel_15/TestRunner.php');
-passthru('php propel_15_with_cache/TestRunner.php');
-passthru('php doctrine_12/TestRunner.php');
-passthru('php doctrine_2/TestRunner.php');
+//passthru('php propel_15/TestRunner.php');
+//passthru('php propel_15_with_cache/TestRunner.php');
+//passthru('php doctrine_12/TestRunner.php');
+//passthru('php doctrine_2/TestRunner.php');
